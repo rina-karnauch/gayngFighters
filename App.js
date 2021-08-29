7c37656d0c62fe546b76fdfce1e3e2dcc642e8df
rina.karnauch <rina.karnauch@mail.huji.ac.il>
2021-08-29 14:52:23 +0300

    kind of finished

--- App.js
+++ App.js
@@ -31,7 +31,11 @@ function showInfo() {
     var select = document.getElementById('select-fighter');
     var option = select.options[select.selectedIndex];
     var image = document.getElementsByClassName("hero-photo")[0];
-    image.src = "images/" + option.value + "-no-background.png";
+    image.src = "images/" + option.value.toLowerCase() + "-no-background.png";
+
+    // change info.json from json
+    readJSON(option.value);
+
 }
 
 function showSelection() {
@@ -40,4 +44,21 @@ function showSelection() {
 
     InfoCard = document.getElementById("information-card");
     InfoCard.style.display = "none";
+}
+
+function readJSON(option) {
+    $.getJSON('information.json', function (data) {
+        for (var i = 0; i < data.length; i++) {
+            var card = data[i];
+            if (card.firstName === option) {
+                console.log("hello");
+                document.getElementsByClassName("hero-name")[0].innerHTML =
+                    card.firstName + " " + card.lastName;
+                document.getElementsByClassName("hero-superpower")[0].innerHTML =
+                    card.superPower;
+                document.getElementsByClassName("hero-info")[0].innerHTML =
+                    card.information;
+            }
+        }
+    });
 }
