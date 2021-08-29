e7bdd39ff19a3bb9720bed1eba9fba5f9b5ef936
rina.karnauch <rina.karnauch@mail.huji.ac.il>
2021-08-29 14:03:49 +0300

    photo on selection

--- App.js
+++ App.js
@@ -1,35 +1,40 @@
-function showFight(option) {
+function showFight() {
     fightbtn = document.getElementById("fight-btn");
     fightbtn.style.display = "inline-block";
 }
 
-function hideFight(option) {
+function hideFight() {
     fightbtn = document.getElementById("fight-btn");
     fightbtn.style.display = "none";
 }
 
-function getSelection(){
+function getSelection() {
     var select = document.getElementById('select-fighter');
     var option = select.options[select.selectedIndex];
 
-    if(option.value){
-        showFight(option);
-    }
-    else{
-        hideFight(option);
+    if (option.value) {
+        showFight();
+    } else {
+        hideFight();
     }
 }
 
-function showInfo(){
+function showInfo() {
 
     selectionBox = document.getElementById("main-content-selection");
     selectionBox.style.display = "none";
 
     InfoCard = document.getElementById("information-card");
     InfoCard.style.display = "block";
+
+    // show picked photo
+    var select = document.getElementById('select-fighter');
+    var option = select.options[select.selectedIndex];
+    var image = document.getElementsByClassName("hero-photo")[0];
+    image.src = "images/" + option.value + "-no-background.png";
 }
 
-function showSelection(){
+function showSelection() {
     selectionBox = document.getElementById("main-content-selection");
     selectionBox.style.display = "inline-block";
 
